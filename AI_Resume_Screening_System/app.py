b03251d09ba215351867d16876a0f34a47e4b0d3
Vaishnavi Veerupakshabhatt Joshi <vaishnavivjoshi290@gmail.com>
2026-07-17 23:30:35 +0530

    Added Job model and updated database

--- AI_Resume_Screening_System/app.py
+++ AI_Resume_Screening_System/app.py
@@ -55,6 +55,15 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
 
+class Job(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(db.String(100))
+
+    company = db.Column(db.String(100))
+
+    skills = db.Column(db.Text)
 
 # -------------------------------
 # Create Database
